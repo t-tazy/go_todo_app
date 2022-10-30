f2e59ca3092ffa680235c29af477f6cbe4629866
t-tazy <takumin390511@gmail.com>
2022-10-30 09:58:34 +0900

    mod tab

--- main.go
+++ main.go
@@ -12,45 +12,45 @@ import (
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        log.Printf("need port number\n")
-        os.Exit(1)
-    }
-    p := os.Args[1] // ポート番号
-    l, err := net.Listen("tcp", ":"+p)
-    if err != nil {
-        log.Fatalf("failed to listen port %s: %v", p, err)
-    }
-    if err := run(context.Background(), l); err != nil {
-        log.Printf("failed to terminate server: %v", err)
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1] // ポート番号
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
 
 func run(ctx context.Context, l net.Listener) error {
-    s := &http.Server{
-        // 引数で受け取ったnet.Listenerを利用するため、Addrフィールドは指定しない
-        Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-        }),
-    }
-    eg, ctx := errgroup.WithContext(ctx)
-    // 別ゴルーチンでHTTPサーバーを起動する
-    eg.Go(func() error {
-        // http.ErrServerCloseは
-        // http.Server.Shutdownが正常に終了したことを示すため異常ではない
-        if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-            log.Printf("failed to close: %+v", err)
-            return err
-        }
-        return nil
-    })
+	s := &http.Server{
+		// 引数で受け取ったnet.Listenerを利用するため、Addrフィールドは指定しない
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		// http.ErrServerCloseは
+		// http.Server.Shutdownが正常に終了したことを示すため異常ではない
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
 
-    // チャネルからの終了通知を待機する
-    <-ctx.Done()
-    if err := s.Shutdown(context.Background()); err != nil {
-        log.Printf("failed to shutdown: %+v", err)
-    }
-    // Goメソッドで起動した別ゴルーチンの終了を待つ
-    return eg.Wait()
+	// チャネルからの終了通知を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別ゴルーチンの終了を待つ
+	return eg.Wait()
 }
