a0c4c0b5599606a4982a435b2c29616631b2267f
t-tazy <takumin390511@gmail.com>
2022-10-31 10:45:36 +0900

    SECTION-061

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/t-tazy/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +21,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	// ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	// defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -33,6 +36,8 @@ func run(ctx context.Context) error {
 	s := &http.Server{
 		// 引数で受け取ったnet.Listenerを利用するため、Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// コマンドラインで実験するため
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
