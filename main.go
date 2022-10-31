f4ce7e538e9f5620804898009c19b432d19bcecd
t-tazy <takumin390511@gmail.com>
2022-11-01 08:46:06 +0900

    SECTION-063

--- main.go
+++ main.go
@@ -20,6 +20,17 @@ func main() {
 	}
 }
 
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+	// HTTPサーバーが稼働中か確認するための/healthエンドポイントを宣言
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+	return mux
+}
+
 func run(ctx context.Context) error {
 	// ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 	// defer stop()
