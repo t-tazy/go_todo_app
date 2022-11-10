7de2982ad56600c36520b68b973a13eefd4d5eaf
t-tazy <takumin390511@gmail.com>
2022-11-10 14:39:41 +0900

    SECTION-075

--- mux.go
+++ mux.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/t-tazy/go_todo_app/clock"
+	"github.com/t-tazy/go_todo_app/config"
 	"github.com/t-tazy/go_todo_app/handler"
 	"github.com/t-tazy/go_todo_app/store"
 )
 
-func NewMux() http.Handler {
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
 	// HTTPサーバーが稼働中か確認するための/healthエンドポイントを宣言
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -18,9 +21,14 @@ func NewMux() http.Handler {
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	r := store.Repository{Clocker: clock.RealClocker{}}
+	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{Store: store.Tasks}
+	lt := &handler.ListTask{DB: db, Repo: &r}
 	mux.Get("/tasks", lt.ServeHTTP)
-	return mux
+	return mux, cleanup, nil
 }
