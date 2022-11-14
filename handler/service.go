a8e895e616cde20bb79c793a470f537a3c4e12a4
t-tazy <takumin390511@gmail.com>
2022-11-15 02:30:28 +0900

    SECTION-078

--- handler/service.go
+++ handler/service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/t-tazy/go_todo_app/entity"
 )
 
-//go:generate go run github.com/motryer/moq -out mot_test.go . ListTasksService AddTaskService
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
 }
